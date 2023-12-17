6bcc8cad11ae55ea93a092c1d6181cccef07260a
Vasilis Nikolaou <b.nikolaou20@gmail.com>
2023-12-17 16:27:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,9 +12,25 @@ class Course:
         self.semester = semester
 
 class Supervisor:
-    def __init__(self, id, name, supervisions):
+    def __init__(self, id, name, supervisions, unavailabilities):
         self.id = id
         self.name = name
-        self,supervisions = supervisions
+        self.supervisions = supervisions
+        self.unavailabilities = unavailabilities
+
+def assign(courses, supervisors):
+    assigned = list()
+    for course in courses:
+        available_sups = [Supervisor for Supervisor in supervisors if Course.id not in Supervisor.unavailabilities]
+        for i in range(Course.supervisors_needed):
+            if len(available_sups) == 0:
+                break
+            assigned_sup = available_sups.pop(0)
+            assigned_sup.supervisions -= 1
+            assigned.append(assigned.id)
+    return assigned
+
+
+
 
         
